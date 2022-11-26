be5499549a5fe0c4756f323239395825510dc06f
Yonatan Addis <yonatanaddis0257@gmail.com>
2022-11-26 10:09:09 +0300

    task_4

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,21 @@ class BaseModel:
     """A class that is use as a Super class (base model) of all our classes.
         it defines all common attributes and methods for other classes to share """
 
-    def __init__(self):
+    def __init__(self,*args, **kwargs):
         """used to construct the state of the object when first created"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for k in kwargs:
+                if (k != '__class__'):
+                    if (k == 'created_at'):
+                        self.__setattr__('created_at', datetime.strptime(kwargs[k], '%Y-%m-%dT%H:%M:%S.%f'))
+                    if (k == 'updated_at'):
+                        self.__setattr__('updated_at', datetime.strptime(kwargs[k], '%Y-%m-%dT%H:%M:%S.%f'))
+                    if (k != 'created_at' and k != 'updated_at'):
+                        self.__setattr__(k, kwargs[k])
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """String representation of the object"""
@@ -29,3 +39,26 @@ class BaseModel:
         new_dict['created_at'] = self.created_at.isoformat()
         new_dict['updated_at'] = self.updated_at.isoformat()
         return new_dict
+
+
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+print(my_model.id)
+print(my_model)
+print(type(my_model.created_at))
+print("--")
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+print("--")
+
+my_new_model = BaseModel(**my_model_json)
+print(my_new_model.id)
+print(my_new_model)
+print(type(my_new_model.created_at))
+print("--")
+print(my_model is my_new_model)
