46911c6f6e68024077bf2c94f55ebd18e0bd7269
Yonatan Addis <yonatanaddis0257@gmail.com>
2022-11-27 18:31:30 +0300

    adding file

--- models/base_model.py
+++ models/base_model.py
@@ -9,10 +9,6 @@ class BaseModel:
     """
     Parent class for AirBnB clone project
     Methods:
-<<<<<<< HEAD
-
-=======
->>>>>>> 2d8db84d3fcb6a86cea270c3792096b5720fb67c
     """
 
     def __init__(self, *args, **kwargs):
@@ -22,17 +18,14 @@ class BaseModel:
 
         if kwargs:
             for key, val in kwargs.items():
-<<<<<<< HEAD
                 if key != "__class__":
                     setattr(self, key, val)
                 if key == "created_at" or key == "updated_at":
                     time_format = "%Y-%m-%dT%H:%M:%S.%f"
-=======
-                if key != '__class__':
+                if key != "__class__":
                     setattr(self, key, val)
-                if key == 'created_at' or key == 'updated_at':
-                    time_format = '%Y-%m-%dT%H:%M:%S.%f'
->>>>>>> 2d8db84d3fcb6a86cea270c3792096b5720fb67c
+                if key == "created_at" or key == "updated_at":
+                    time_format = "%Y-%m-%dT%H:%M:%S.%f"
                     setattr(self, key, datetime.strptime(val, time_format))
         else:
             self.id = str(uuid4())
@@ -50,19 +43,15 @@ class BaseModel:
         models.storage.save()
 
     def to_dict(self):
-<<<<<<< HEAD
         """return dictionary containing all key value"""
 
         di = self.__dict__.copy()
         di["__class__"] = self.__class__.__name__
         di["created_at"] = datetime.isoformat(self.created_at)
         di["updated_at"] = datetime.isoformat(self.updated_at)
-=======
         """return dictionary containing all key value """
-
         di = self.__dict__.copy()
-        di['__class__'] = self.__class__.__name__
-        di['created_at'] = datetime.isoformat(self.created_at)
-        di['updated_at'] = datetime.isoformat(self.updated_at)
->>>>>>> 2d8db84d3fcb6a86cea270c3792096b5720fb67c
+        di["__class__"] = self.__class__.__name__
+        di["created_at"] = datetime.isoformat(self.created_at)
+        di["updated_at"] = datetime.isoformat(self.updated_at)
         return di
