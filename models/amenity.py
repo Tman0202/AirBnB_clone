bc6e2249123807a5adf5917881cf0c088974668a
Yonatan Addis <yonatanaddis0257@gmail.com>
2022-11-27 20:34:27 +0300

    updating

--- models/amenity.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Module that inherits from BaseModel"""
-from models.base_model import BaseModel
-
-
-class Amenity(BaseModel):
-    """class the represent Amenity"""
-    name = ""
